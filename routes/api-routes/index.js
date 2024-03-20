cab3585c19b19ac5148c56b6a4542bfa710f4abd
chrisnastro <cnastro@icloud.com>
2024-03-19 22:56:59 -0400

    built api routes index

---
+++ routes/api-routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const commentRoutes = require('./comment-routes');
+
+router.use('/user', userRoutes);
+router.use('/comment', commentRoutes);
+
+module.exports = router;
