fab7c413b96c36412baa4b284e7709c7cb52b3ac
chrisnastro <cnastro@icloud.com>
2024-03-20 08:13:40 -0400

    finished comment controller

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -23,6 +23,60 @@ const CommentController = {
         }
     },
     async createComment(req, res) {
-        
-    }
-}
+        try {
+            const comment = await Comment.create(req.body);
+            res.status(201).json(comment);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteComment(req, res) {
+        try {
+            const comment = await Comment.findByIdAndDelete({ _id: req.params.commentId });
+            res.status(200).json(comment);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateCommentById(req, res) {
+        try {
+            const comment = await Comment.findByIdAndUpdate(req.params.commentId, req.body, {
+                new: true,
+            });
+            if (!comment) {
+                res.status(404).json({ message: "Comment Not Found!" });
+            } else {
+                res.json(comment);
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createFeedback(req, res) {
+        try {
+            const comment = await Comment.findOneAndUpdate(
+                {_id: req.params.commentId},
+                {$addtoSet: {feedback: req.body}},
+                {runValidators: true, new: true}
+            );
+            comment ? res.json(comment) : res.status(404).json({message: notFound});
+        }catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteFeedback(req, res) {
+        try {
+            const comment = await Comment.findOneAndUpdate(
+                {_id: req.params.commentId},
+                {$pull: {feedback: {feedbackId: req.params.feedbackId}}},
+                {runValidators: true, new: true}
+            );
+            comment ? res.json(comment) : res.status(404).json({message: notFound});
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+};
+
+module.exports = CommentController;
